08b411777e896c1108611547040cd64ebf2337e6
Webnai <wilbertboadzo144@gmail.com>
2023-05-14 02:33:40 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-"""Module for FileStorage class."""
 import datetime
 import json
 import os
@@ -7,6 +5,7 @@ import os
 
 class FileStorage:
     """Class for serialization and deserialization of base classes."""
+
     __file_path = "file.json"
     __objects = {}
 
@@ -22,8 +21,22 @@ class FileStorage:
     def save(self):
         """Serializes __objects to JSON file."""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-            json.dump(d, f)
+            obj_dict = {}
+            for key, obj in FileStorage.__objects.items():
+                obj_dict[key] = obj.to_dict()
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes JSON file into __objects."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            for key, obj_data in obj_dict.items():
+                class_name, obj_id = key.split('.')
+                obj_class = self.classes()[class_name]
+                obj = obj_class(**obj_data)
+                self.new(obj)
 
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
@@ -44,52 +57,47 @@ class FileStorage:
                    "Review": Review}
         return classes
 
-    def reload(self):
-        """Deserializes JSON file into __objects."""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            FileStorage.__objects = obj_dict
-
-    def attributes(self, classname=None):
+    def attributes(self):
         """Returns the valid attributes and their types for classname."""
         attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-                     {"place_id": str,
-                      "user_id": str,
-                      "text": str}
+            "BaseModel": {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime
+            },
+            "User": {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str
+            },
+            "State": {
+                "name": str
+            },
+            "City": {
+                "state_id": str,
+                "name": str
+            },
+            "Amenity": {
+                "name": str
+            },
+            "Place": {
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list
+            },
+            "Review": {
+                "place_id": str,
+                "user_id": str,
+                "text": str
+            }
         }
-        if classname:
-            return attributes[classname]
         return attributes
