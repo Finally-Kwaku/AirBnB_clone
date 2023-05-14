e82917074921771c1b2734e29fa27efb8fcda322
Webnai <wilbertboadzo144@gmail.com>
2023-05-14 02:26:01 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""This is the File Storage class module."""
+"""Module for FileStorage class."""
 import datetime
 import json
 import os
 
 
 class FileStorage:
-
-    """Class for serializtion and deserialization of base classes."""
+    """Class for serialization and deserialization of base classes."""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns __objects dictionary."""
-        # TODO: should this be a copy()?
-        return FileStorage.__objects
+        return FileStorage.__objects.copy()
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
-        # TODO: should these be more precise specifiers?
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serialzes __objects to JSON file."""
+        """Serializes __objects to JSON file."""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
@@ -55,10 +52,9 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
-    def attributes(self):
+    def attributes(self, classname=None):
         """Returns the valid attributes and their types for classname."""
         attributes = {
             "BaseModel":
@@ -90,8 +86,10 @@ class FileStorage:
                       "longitude": float,
                       "amenity_ids": list},
             "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str}
         }
+        if classname:
+            return attributes[classname]
         return attributes
